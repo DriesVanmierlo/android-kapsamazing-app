e303cad37dde00f99ef939a8b13a1a270f0fca93
DriesVanmierlo <dries.vanmierlo@student.ehb.be>
2022-05-18 09:22:30 +0200

    WIP start of recyclerView on homepage

--- app/src/main/java/org/vanmierlo/kapsamazing_app/ui/home/HomeViewModel.kt
+++ app/src/main/java/org/vanmierlo/kapsamazing_app/ui/home/HomeViewModel.kt
@@ -3,6 +3,7 @@ package org.vanmierlo.kapsamazing_app.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.android.volley.toolbox.JsonObjectRequest
 
 class HomeViewModel : ViewModel() {
 
@@ -10,4 +11,18 @@ class HomeViewModel : ViewModel() {
         value = "This is home Fragment"
     }
     val text: LiveData<String> = _text
+
+    val url = "http://my-json-feed"
+
+//    val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
+//        Response.Listener { response ->
+//            textView.text = "Response: %s".format(response.toString())
+//        },
+//        Response.ErrorListener { error ->
+//            // TODO: Handle error
+//        }
+//    )
+
+// Access the RequestQueue through your singleton class.
+//    MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
 }
