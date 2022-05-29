5e607aed66d6f555b11b0df319e03115a8cbb035
DriesVanmierlo <dries.vanmierlo@student.ehb.be>
2022-05-29 16:35:56 +0200

    Added dutch support for detail page

---
+++ app/src/main/java/org/vanmierlo/kapsamazing_app/RatingActivity.kt
@@ -0,0 +1,11 @@
+package org.vanmierlo.kapsamazing_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RatingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rating)
+    }
+}
---
+++ app/src/main/java/org/vanmierlo/kapsamazing_app/util/Constants.kt
@@ -0,0 +1,6 @@
+package org.vanmierlo.kapsamazing_app.util
+
+public class Constants {
+    private val MAPVIEW_BUNDLE_KEY = "MapViewBundleKey"
+
+}
